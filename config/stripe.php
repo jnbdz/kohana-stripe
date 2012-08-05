62fc0a23427eb78f08a96d0658cf474d73a07dcd
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2012-08-05 05:04:10 -0400

    first commit

---
+++ config/stripe.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+        'test' => array(
+                'secret_key' => 'YOUR_KEY',
+                'publishable_key' => 'YOUR_KEY',
+        ),
+        'live' => array(
+                'secret_key' => 'YOUR_KEY',
+                'publishable_key' => 'YOUR_KEY',
+        ),
+);
---
+++ init.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+require_once Kohana::find_file('vendor', 'stripe-php/lib/Stripe', 'php');
