606f5e6608dde689e829575ccb0a7cce1d512b02
Jean-Nicolas Boulay <contact@jean-nicolas.com>
2012-08-07 16:06:10 -0400

    Added status setting in the stripe config file.

--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
+	'status' => 'test',
         'test' => array(
                 'secret_key' => 'YOUR_KEY',
                 'publishable_key' => 'YOUR_KEY',
