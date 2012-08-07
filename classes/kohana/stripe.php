5d7d1e60c28f0ecbd66e68d328c707154483c728
Jean-Nicolas Boulay <contact@jean-nicolas.com>
2012-08-07 17:18:14 -0400

    Added classes with a new init method.

--- classes/kohana/stripe.php
+++ classes/kohana/stripe.php
@@ -10,67 +10,22 @@
 
 class Kohana_STRIPE {
 
+	protected static $private_key = NULL;
+	protected static $publishable_key = NULL;
+
 	public static function init()
 	{
 		if (self::$private_key != null && self::$publishable_key != null) {
 			return;
 		}
 
+		require_once Kohana::find_file('vendor', 'stripe-php/lib/Stripe', 'php');
+
 		$config = Kohana::config('stripe');
 		self::$private_key = $config['private_key'];
 		self::$publishable_key = $config['publishable_key'];
 
 		Stripe::setApiKey(self::$private_key);
-	}
-
-	/**
-	 * Charge section
-	 */
-
-	public static function charge_create($data)
-	{
-		self::init();
-		return Stripe_Charge::create($data);
-	}
-
-	public static function charge_retrieve($charge_id)
-	{
-		self::init();
-		return Stripe_Charge::retrieve($charge_id);
-	}
-
-	public static function charge_all($settings=array())
-	{
-		self::init();
-		return Stripe_Charge::all($settings);
-	}
-
-	/**
-	 * Customers section
-	 *
-	 * Customer objects allow you to perform recurring charges
-	 * and track multiple charges that are associated with the
-	 * same customer. The API allows you to create, delete,
-	 * and update your customers. You can retrieve individual
-	 * customers as well as a list of all your customers.
-	 */
-
-	public static function customer_create($data)
-        {
-                self::init();
-                return Stripe_Customer::create($data);
-        }
-
-	public static function customer_retrieve($customer_id)
-	{
-		self::init();
-		return Stripe_Customer::retrieve($customer_id);
-	}
-
-	public static function customer_all($settings=array())
-	{
-		self::init();
-		return Stripe_Customer::all($settings);
 	}
 
 } // End of Stripe
